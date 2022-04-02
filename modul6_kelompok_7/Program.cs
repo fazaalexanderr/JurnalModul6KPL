90625c17938d6febf255fe3fedfe72b937c417d9
fzTelU <fazaalexander@student.telkomuniversity.ac.id>
2022-04-02 09:39:04 +0700

    inisialisasi project

---
+++ modul6_kelompok_7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace modul6_kelompok_7
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
