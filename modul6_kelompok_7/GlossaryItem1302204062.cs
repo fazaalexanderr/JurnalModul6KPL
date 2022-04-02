dd7824c5a9cde2c3053ae30ec89abda005d622be
fzTelU <fazaalexander@student.telkomuniversity.ac.id>
2022-04-02 11:48:35 +0700

    menambahkan class GlossaryItem1302204062

---
+++ modul6_kelompok_7/GlossaryItem1302204062.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace modul6_kelompok_7
+{
+    public class GlossaryItem1302204062
+    {
+        public Glossary glossary { get; set; }
+
+        public void ReadJSON()
+        {
+            string jsonString = File.ReadAllText("/Users/Faza/praktikum_kpl/modul6_kelompok_7/modul6_kelompok_7/jurnal6_3_1302204062.json");
+
+            GlossaryItem1302204062 item = JsonSerializer.Deserialize<GlossaryItem1302204062>(jsonString);
+
+            Console.WriteLine("ID: " + item.glossary.GlossDiv.GlossList.GlossEntry.ID);
+            Console.WriteLine("SortAs: " + item.glossary.GlossDiv.GlossList.GlossEntry.SortAs);
+            Console.WriteLine("GlossTerm: " + item.glossary.GlossDiv.GlossList.GlossEntry.GlossTerm);
+            Console.WriteLine("Acronym: " + item.glossary.GlossDiv.GlossList.GlossEntry.Acronym);
+            Console.WriteLine("Abbrev: " + item.glossary.GlossDiv.GlossList.GlossEntry.Abbrev);
+            Console.WriteLine("GlossDef: ");
+            Console.WriteLine("para: " + item.glossary.GlossDiv.GlossList.GlossEntry.GlossDef.para);
+            Console.WriteLine("GlossSeeAlso: ");
+            foreach (string gloss in item.glossary.GlossDiv.GlossList.GlossEntry.GlossDef.GlossSeeAlso)
+            {
+                Console.WriteLine(gloss);
+            }
+            Console.WriteLine("GlossSee: " + item.glossary.GlossDiv.GlossList.GlossEntry.GlossSee);
+        }
+        public GlossaryItem1302204062() { }
+    }
+
+    public class Glossary
+    {
+        public string title { get; set; }
+        public GD GlossDiv { get; set; }
+    }
+
+    public class GD
+    {
+        public string title { get; set; }
+        public GL GlossList { get; set; }
+    }
+    public class GL
+    {
+        public GE GlossEntry { get; set; }
+    }
+    public class GE
+    {
+        public string ID { get; set; }
+        public string SortAs { get; set; }
+        public string GlossTerm { get; set; }
+        public string Acronym { get; set; }
+        public string Abbrev { get; set; }
+        public GDef GlossDef { get; set; }
+        public string GlossSee { get; set; }
+    }
+    public class GDef
+    {
+        public string para { get; set; }
+        public List<string> GlossSeeAlso { get; set; }
+    }
+}
--- modul6_kelompok_7/Program.cs
+++ modul6_kelompok_7/Program.cs
@@ -8,8 +8,13 @@ namespace modul6_kelompok_7
         {
             DataMahasiswa1302204062 faza = new DataMahasiswa1302204062();
             faza.ReadJSON();
+            Console.WriteLine("");
             TeamMembers1302204062 team = new TeamMembers1302204062();
             team.ReadJSON();
+            Console.WriteLine("");
+            GlossaryItem1302204062 item = new GlossaryItem1302204062();
+            item.ReadJSON();
+            Console.WriteLine("");
         }
     }
 }
