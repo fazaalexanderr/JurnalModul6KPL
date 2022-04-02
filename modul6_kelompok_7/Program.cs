a820603182390cff64d4c380d817b53b9d83c27d
fzTelU <fazaalexander@student.telkomuniversity.ac.id>
2022-04-02 11:14:58 +0700

    menambahkan class TeamMembers1302204062

--- modul6_kelompok_7/Program.cs
+++ modul6_kelompok_7/Program.cs
@@ -8,6 +8,8 @@ namespace modul6_kelompok_7
         {
             DataMahasiswa1302204062 faza = new DataMahasiswa1302204062();
             faza.ReadJSON();
+            TeamMembers1302204062 team = new TeamMembers1302204062();
+            team.ReadJSON();
         }
     }
 }
---
+++ modul6_kelompok_7/TeamMembers1302204062.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace modul6_kelompok_7
+{
+    public class TeamMembers1302204062
+    {
+        public List<Member> members { get; set; }
+
+        public void ReadJSON()
+        {
+            string jsonString = File.ReadAllText("/Users/Faza/praktikum_kpl/modul6_kelompok_7/modul6_kelompok_7/jurnal6_2_1302204062.json");
+
+            TeamMembers1302204062 team = JsonSerializer.Deserialize<TeamMembers1302204062>(jsonString);
+
+            Console.WriteLine("Team member list:");
+            int i = 0;
+            foreach(Member anggota in team.members)
+            {
+                i++;
+                Console.WriteLine(anggota.nim + " " + anggota.firstName + " " + anggota.lastName + " " + anggota.age + " " + anggota.gender);
+            }
+        }
+        public TeamMembers1302204062() { }
+    }
+
+    public class Member
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public string nim { get; set; }
+    }
+}
